f9a8ef4089f7876e8562e58c763bf4f4f68a98dc
Akhtungg <indeks_dou-jonsa@mail.ru>
2025-03-16 20:08:25 +0300

    chore: redundant database entities have been removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './infrastructure/modules/user.module';
 import { TypeOrmComponent } from './infrastructure/db/conect';
 import { AuthModule } from './infrastructure/modules/auth.module';
-import { ApplicationModule } from './infrastructure/modules/application.module';
-import { StudentModule } from './infrastructure/modules/student.module';
-//import { RolesGuard } from 'src/infrastructure/JWT/guards/roles.guard'
+
 
 @Module({
   imports: [
@@ -15,8 +13,6 @@ import { StudentModule } from './infrastructure/modules/student.module';
     TypeOrmComponent,
     UserModule,
     AuthModule,
-    ApplicationModule,
-    StudentModule,
   ],
   controllers: [],
   providers: [],
